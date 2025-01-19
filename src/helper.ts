1481bb5defe08648013691922add7e6e08e324c6
Stephan-Stieger <stephanstieger2002@gmail.com>
2025-01-19 14:28:07 +0100

    small changes,refactoring, working on the project.astro

--- src/helper.ts
+++ src/helper.ts
@@ -1,5 +1,9 @@
-interface String {
-    removePart(partToRemove: string): string;
+import {parse} from "node-html-parser";
+
+declare global {
+    interface String {
+        removePart(partToRemove: string): string;
+    }
 }
 
 String.prototype.removePart = function (partToRemove: string): string {
@@ -15,4 +19,33 @@ function getPathOfAListOfComponents(allPages:any):string[]{
         paths.push(path)
     })
     return paths
+}
+
+async function getPathAndTitleOfAListOfComponents(allPages: any): Promise<{ path: string, title: string }[]> {
+    let pathsAndTitles: { path: string, title: string }[] = [];
+    allPages.map((item:any) => {
+        let fullPath=item.url
+        fullPath=fullPath.removePart('.mdx')
+        let path=fullPath.split("/").pop()
+        console.log(item)
+        pathsAndTitles.push({path: path, title: ""})
+    })
+    return pathsAndTitles;
+}
+
+function getPathFromModuleId(moduleId:string):string{
+    let path=moduleId.split("/").pop()
+    if (path===undefined || path.includes('index')){
+        return ""
+    }
+    else {
+        return path.removePart('.astro')
+    }
+
+}
+
+export {
+    getPathOfAListOfComponents,
+    getPathAndTitleOfAListOfComponents,
+    getPathFromModuleId
 }
